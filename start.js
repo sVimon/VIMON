ac103ae145f1462b7fe17d665ca8f8f3ad569628
sVimon <yassinyass8788@gmail.com>
2026-01-25 21:30:29 +0100

    Update start.js

--- start.js
+++ start.js
@@ -7,6 +7,7 @@ document.addEventListener('DOMContentLoaded', () => {
     // قائمة الـ IDs المسموح بها (أضف أرقامك هنا)
     const WHITELIST = [
         "1528434705",
+        "1543129947",
         "1541497569",
         "1540611951",
         "1543193547",
@@ -254,6 +255,7 @@ document.addEventListener('DOMContentLoaded', () => {
     });
 
 });
+
 
 
 
